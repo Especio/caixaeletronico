305025e655a17489ea696168c2f440ed524f6d69
Camila Espécio <especioespecio@gmail.com>
2023-06-17 13:00:14 -0300

    projeto do caixa eletronico codebridge

---
+++ src/transacoes/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+
+
+class DepositarForm(forms.Form):
+    cliente = forms.IntegerField(label="ID do cliente", required=True)
+    valor = forms.DecimalField(label="valor", required=True)
+
+class DebitarForm(DepositarForm):
+    pass
